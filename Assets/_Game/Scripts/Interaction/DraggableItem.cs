ffc761c7741e7e0907dfcd9b858d539f5fb01e47
Yashneil Narain Srivastava <yashneil.narain@gmail.com>
2026-02-16 23:56:25 +0530

    Draggable Items Updated

--- Assets/_Game/Scripts/Interaction/DraggableItem.cs
+++ Assets/_Game/Scripts/Interaction/DraggableItem.cs
@@ -1,31 +1,94 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+using System.Collections.Generic;
+
+public class DraggableItem : MonoBehaviour
 {
-    [HideInInspector] public Transform parentAfterDrag;
-    public Image image;
-    public void OnBeginDrag(PointerEventData eventData)
+    public Canvas canvas;
+
+    private GameObject selectedItem;
+    private Transform originalParent;
+    private Vector3 originalPosition;
+
+    void Update()
     {
-        Debug.Log("Begin Drag");
-        parentAfterDrag = transform.parent;
-        transform.SetParent(parentAfterDrag.root);
-        transform.SetAsLastSibling();
-        image.raycastTarget = false;
+        if (Input.GetMouseButtonDown(0))
+        {
+            TrySelectItem();
+        }
+
+        if (Input.GetMouseButton(0) && selectedItem != null)
+        {
+            selectedItem.transform.position = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0) && selectedItem != null)
+        {
+            TryPlaceItem();
+        }
     }
 
-    public void OnDrag(PointerEventData eventData)
+    void TrySelectItem()
     {
-        Debug.Log("Dragging");
-        transform.position = Input.mousePosition;
+        PointerEventData data = new PointerEventData(EventSystem.current);
+        data.position = Input.mousePosition;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(data, results);
+
+        foreach (RaycastResult result in results)
+        {
+            Image img = result.gameObject.GetComponent<Image>();
+            if (img != null && result.gameObject.CompareTag("Item"))
+            {
+                selectedItem = result.gameObject;
+                originalParent = selectedItem.transform.parent;
+                originalPosition = selectedItem.transform.position;
+
+                selectedItem.transform.SetParent(canvas.transform);
+                break;
+            }
+        }
     }
 
-    public void OnEndDrag(PointerEventData eventData)
+    void TryPlaceItem()
     {
-        Debug.Log("End Drag");
-        transform.SetParent(parentAfterDrag);
-        image.raycastTarget = true;
+        PointerEventData data = new PointerEventData(EventSystem.current);
+        data.position = Input.mousePosition;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(data, results);
+
+        bool placed = false;
+
+        foreach (RaycastResult result in results)
+        {
+            if (result.gameObject.CompareTag("Slot"))
+            {
+                Transform slot = result.gameObject.transform;
+
+                if (slot.GetComponentInChildren<Image>())
+                {
+                    Transform otherItem = slot.GetChild(0);
+                    otherItem.SetParent(originalParent);
+                    otherItem.localPosition = Vector3.zero;
+
+                    selectedItem.transform.SetParent(slot);
+                    selectedItem.transform.localPosition = Vector3.zero;
+                }
+
+                placed = true;
+                break;
+            }
+        }
+
+        if (!placed)
+        {
+            selectedItem.transform.SetParent(originalParent);
+            selectedItem.transform.position = originalPosition;
+        }
+
+        selectedItem = null;
     }
 }
--- Assets/_Game/Scripts/Interaction/InventorySlot.cs
+++ Assets/_Game/Scripts/Interaction/InventorySlot.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-public class InventorySlot : MonoBehaviour, IDropHandler
-{
-    public void OnDrop(PointerEventData eventData)
-    {
-        GameObject drop = eventData.pointerDrag;
-        DraggableItem draggableItem;
-        if (!gameObject.GetComponentInChildren<Image>().IsActive())
-        {
-            draggableItem = drop.GetComponent<DraggableItem>();
-            draggableItem.parentAfterDrag = transform;
-        }
-    }
-}
+//public class InventorySlot : MonoBehaviour, IDropHandler
+//{
+//    //public void OnDrop(PointerEventData eventData)
+//    //{
+//    //    GameObject drop = eventData.pointerDrag;
+//    //    DraggableItem draggableItem;
+//    //    if (!gameObject.GetComponentInChildren<Image>().IsActive())
+//    //    {
+//    //        draggableItem = drop.GetComponent<DraggableItem>();
+//    //        draggableItem.parentAfterDrag = transform;
+//    //    }
+//    //}
+//}
