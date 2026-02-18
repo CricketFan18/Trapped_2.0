2ee377b5b669152ade73585ebf50e229cc128505
JACKARPANJACK <134756746+JACKARPANJACK@users.noreply.github.com>
2026-02-18 19:29:12 +0530

    Remove redundant code from game manager and fixed merge conflict

--- Assets/_Game/Scripts/Player/FirstPersonController.cs
+++ Assets/_Game/Scripts/Player/FirstPersonController.cs
@@ -66,8 +66,6 @@ public class FirstPersonController : MonoBehaviour
 
     private void Update()
     {
-        if (GameManager.Instance != null && GameManager.Instance.IsGamePaused) return;
-
         if (IsFocused)
         {
             UpdateFocusMode();
